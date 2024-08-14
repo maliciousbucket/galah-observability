e3ce89aeac7fbc6eeb8f207807d3684068a5a1fa
maliciousbucket <85473030+maliciousbucket@users.noreply.github.com>
2024-08-14 16:35:29 +1000

    update tool scripts

---
+++ tools/util/environment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+read_env() {
+  local filePath="${1:-.env}"
+
+  if [ ! -f "$filePath" ]; then
+    echo "missing ${filePath}"
+    exit 1
+  fi
+
+  log "Reading $filePath"
+  while read -r LINE; do
+    # Remove leading and trailing whitespaces, and carriage return
+    CLEANED_LINE=$(echo "$LINE" | awk '{$1=$1};1' | tr -d '\r')
+
+    if [[ $CLEANED_LINE != '#'* ]] && [[ $CLEANED_LINE == *'='* ]]; then
+      export "$(CLEANED_LINE)"
+    fi
+  done < "$filePath"
+}
+
